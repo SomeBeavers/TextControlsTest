360bdf85418f1a8d7b0297b70855fbc5d47380b6
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-23 12:48:25 +0200

    Add projects.

---
+++ Multi/Multi1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Multi
+{
+    public class Multi1
+    {
+        // comment is here
+
+#if NET6_0
+        public void Test()
+        {
+            Console.WriteLine();
+        }
+#else
+        public void Test2()
+        {
+            int t = 1;
+            Console.WriteLine(t);
+        }
+
+#endif
+    }
+}
---
+++ Shared/Shared 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared
+{
+    internal class Shared_1
+    {
+        public int MyField { get; private set; }
+
+        public void Test()
+        {
+            Console.WriteLine();
+        }
+
+#if NET6_0
+        public void MethodForNet6()
+        {
+            MyField = 1;
+        }
+#endif
+    }
+}
