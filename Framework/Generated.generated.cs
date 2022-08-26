b68e65b889842f87857fad154b66ac478d51c5e8
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-26 15:46:27 +0200

    Add files.

---
+++ Framework/Generated.generated.cs
@@ -0,0 +1,12 @@
+﻿namespace Framework;
+
+public class Generated_generated
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = value;
+    }
+}
